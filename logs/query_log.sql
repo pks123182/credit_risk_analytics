86c60fa23aa2ec614c88078d3f8539aa3be51139
Pavan Sista <pks123182@gmail.com>
2026-06-08 22:44:56 -0700

    infrastructure: ignore package clutter and add dbt core frameworks

--- logs/query_log.sql
+++
@@ -1,9 +0,0 @@
--- created_at: 2026-06-08T23:20:16.368167+00:00
--- finished_at: 2026-06-08T23:20:16.553303+00:00
--- elapsed: 185ms
--- outcome: success
--- dialect: snowflake
--- node_id: not available
--- query_id: 01c4ebf8-0309-3b09-002c-dcd70011c2ea
--- desc: not provided
-select 1 as id;
